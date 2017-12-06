65669a367d02dea6e7a49916788a90bca5bf6889
pgmkkh <pgm.kkh@gmail.com>
2017-12-06 17:29:12 +0900

    spinner
    12.6 6pm

--- app/src/main/java/com/tistory/pgmkkh/bunkerapp/FirstView.java
+++ app/src/main/java/com/tistory/pgmkkh/bunkerapp/FirstView.java
@@ -4,13 +4,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,7 +25,7 @@ public class FirstView extends Activity {
     private ListProductAdapter adapter;
     private List<Product> mProductList;
     private DatabaseHelper mDBHelper;
-
+    private Spinner spinner;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -44,6 +47,31 @@ public class FirstView extends Activity {
         mProductList = mDBHelper.getListProduct();
         adapter = new ListProductAdapter(this, mProductList);
         lvProduct.setAdapter(adapter);
+        spinner = (Spinner)findViewById(R.id.spinner);
+
+        final ArrayList<String> list = new ArrayList<>();
+        list.add("지역");
+        list.add("서울특별시");
+        list.add("경기도");
+        list.add("강원도");
+        list.add("경상남도");
+        list.add("경상북도");
+        list.add("경상남도");
+        list.add("광주광역시");
+        list.add("대구광역시");
+        list.add("대전광역시");
+        list.add("부산광역시");
+        list.add("울산광역시");
+        list.add("인천광역시");
+        list.add("전라남도");
+        list.add("전라북도");
+        list.add("제주도");
+        list.add("충청남도");
+        list.add("충청북도");
+
+        ArrayAdapter spinnerAdapter;
+        spinnerAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, list);
+        spinner.setAdapter(spinnerAdapter);
 }
 
     private boolean copyDatabase(Context context) {
